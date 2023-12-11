603ee9ba2fdaca2cf2d2f307424f85daf2b98e47
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-11 09:22:26 +0100

    c programming

--- abc.c
+++ abc.c
@@ -1,14 +1,31 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     int *p;
+//     a = 10;
+//     p = &a;
+//     *p = 12;
+//     int b = 20;
+//     *p = b;
+//     printf("%d\n", p);
+//     printf("%d\n", *p);
+//     printf("%d\n", &a);
+
+
+
+// }
+
 #include <stdio.h>
+
 int main()
 {
-    int a;
+    int a = 1025;
     int *p;
-    a = 10;
     p = &a;
-    printf("%d\n", p);
-    printf("%d\n", *p);
-    printf("%d\n", &a);
+    printf("Size of integer is %ld\n",sizeof(int));
+    printf("%p, %d\n", p, *p);
 
 
-
+    return 0;
 }
