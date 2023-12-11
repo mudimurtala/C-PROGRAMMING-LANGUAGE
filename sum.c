09b8dbafec2ba85a4e352d078e5c8bdd16ab30ee
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-11 08:15:49 +0100

    c programming

--- sum.c
+++ sum.c
@@ -1,44 +1,45 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    // Declare variables
-    int num1, num2, sum;
+// int main() {
+//     // Declare variables
+//     int num1, num2, sum;
 
-    // Get input from the user
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+//     // Get input from the user
+//     printf("Enter the first number: ");
+//     scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+//     printf("Enter the second number: ");
+//     scanf("%d", &num2);
 
-    // Calculate the sum
-    sum = num1 + num2;
+//     // Calculate the sum
+//     sum = num1 + num2;
 
-    // Display the result
-    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+//     // Display the result
+//     printf("The sum of %d and %d is: %d\n", num1, num2, sum);
 
-    return 0;
-}
-
-
-
-int A[MAX SIZE];
-int end = -1;
-insert(2)            remove(0);
-insert(4)            
-insert(5)
-insert(6)
-insert(7)
-insert(9)
+//     return 0;
+// }
 
 
-int a;
-int *p;
-p = &a;
-a = 5;
-print p = add of a;
-print &a = add of a;
-print &p = add od p;
-print *p = value of a; // dereferencing
-*p = 8;
-print *p // 8
+
+// int A[MAX SIZE];
+// int end = -1;
+// insert(2)            remove(0);
+// insert(4)            
+// insert(5)
+// insert(6)
+// insert(7)
+// insert(9)
+
+
+// int a;
+// int *p;
+// p = &a;
+// a = 5;
+// print p = add of a;
+// print &a = add of a;
+// print &p = add od p;
+// print *p = value of a; // dereferencing
+// *p = 8;
+// print *p // 8
+
