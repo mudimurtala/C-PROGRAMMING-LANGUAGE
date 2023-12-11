09eb87cfcf28d5d89fa7e1242464307f0748fc95
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-11 08:15:12 +0100

    c programming

--- sum.c
+++ sum.c
@@ -19,3 +19,26 @@ int main() {
 
     return 0;
 }
+
+
+
+int A[MAX SIZE];
+int end = -1;
+insert(2)            remove(0);
+insert(4)            
+insert(5)
+insert(6)
+insert(7)
+insert(9)
+
+
+int a;
+int *p;
+p = &a;
+a = 5;
+print p = add of a;
+print &a = add of a;
+print &p = add od p;
+print *p = value of a; // dereferencing
+*p = 8;
+print *p // 8
