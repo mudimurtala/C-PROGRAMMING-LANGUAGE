d725661b14152ac54bd434104e3bd6d7b5eb3ad8
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-14 05:07:00 +0100

    c programming

--- abc.c
+++ abc.c
@@ -73,14 +73,14 @@ void main()
 {
     int x = 5, y = 7;
     fun(x, y);
-    printf("Inside main(calling function)");
-    printf("x = %d y = %d", x,y);
+    printf("Inside main(calling function)\n");
+    printf("x = %d y = %d\n", x,y);
 }
 
 void fun(int x, int y)
 {
     x = 7;
     y = 5;
-    printf("Inside fun(called function)");
-    printf("x = %d y = %d", x,y)
+    printf("Inside fun(called function)\n");
+    printf("x = %d y = %d\n", x,y);
 }
