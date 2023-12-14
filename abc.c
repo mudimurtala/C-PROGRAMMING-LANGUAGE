3b5fcbe9f1dfccf211bc2a442426de6a7ac3cd20
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-14 14:59:16 +0100

    c programming

--- abc.c
+++ abc.c
@@ -190,3 +190,24 @@
 //     printf("%d\n", i + j);
 // }
 
+#include <stdio.h>
+
+int arg(int[], int);
+
+void main()
+{
+    int average;
+    int marks[5] = {10, 15, 20, 30, 45};
+    average = arg(marks, 5);
+    printf("arg is %d", average);
+}
+
+int arg(int marks[], int a)
+{
+    int i, sum = 0, average = 0;
+    for(i = 0;i < a; i++)
+    {
+        sum = sum + marks[i];
+        average = sum/a;
+    }
+}
