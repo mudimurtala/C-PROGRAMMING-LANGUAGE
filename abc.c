69f82f122b984d02ecdbcf0081560d38e813ed14
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-14 17:27:09 +0100

    c programming

--- abc.c
+++ abc.c
@@ -215,11 +215,23 @@
 
 #include <stdio.h>
 
-int avg(int[]);
+int avg(int[], int);
 
 void main()
 {
-    int marks[5] = {10, 20, 30, 40, 50}, size;
+    int marks[5] = {10, 20, 30, 40, 50}, size, average;
     size = sizeof(marks)/sizeof(marks[0]);
-    printf("%d\n", size);
+    average = avg(marks, size);
+    printf("average = %d\n", average);
+}
+
+int avg(int marks1[], int size)
+{
+    int i, sum, average;
+    for(i = 0; i < size; i++)
+    {
+        sum = sum + marks1[1];
+    }
+    average = sum/size;
+    return average;
 }
