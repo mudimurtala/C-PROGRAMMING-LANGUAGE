fa6e326a192413dc4ec4f289b912f9cf05bb00fd
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-14 04:20:12 +0100

    c programming

--- abc.c
+++ abc.c
@@ -32,5 +32,20 @@
 
 #include <stdio.h>
 
+void sum()
+{
+    int a, b, sum = 0;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("Sum = %d", sum);
+}
 
+void main()
+{
+    sum();
+    printf("Hello");
+    sum();
+    sum();
+}
 
