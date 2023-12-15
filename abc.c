a7f5001bb015fa50f5b8e32c417b24297fe317e7
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 15:35:57 +0100

    c programming

--- abc.c
+++ abc.c
@@ -440,4 +440,17 @@
 //     return 0;
 // }
 
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
+    int *a = malloc(sizeof(int) * 10);
+
+    for (int i = 0; i < 10; i++)
+    a[i] = 10 - 1;
+
+    for (int i = 0; i < 10; i++)
+    printf("a[%d] = %d\n", i, a[i]);
+    return 0;
+}
