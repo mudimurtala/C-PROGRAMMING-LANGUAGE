73c07b53cbb4659b856825e992d288c7ed85c69b
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 07:39:31 +0100

    c programming

--- abc.c
+++ abc.c
@@ -289,3 +289,16 @@
 
 #include <stdio.h>
 
+char* display();
+
+void main()
+{
+    char* str;
+    str = display();
+    printf("String is : %s\n", str);
+}
+
+char* display()
+{
+    return "jenny";
+}
