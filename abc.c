759cd0e6014a84d9816e2206e0355e4986247dc3
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 08:15:08 +0100

    c programming

--- abc.c
+++ abc.c
@@ -305,19 +305,22 @@
 //     return str;
 // }
 
+// #include <stdio.h>
+
+// int* returnPointer(int[]);
+
+// void main()
+// {
+//     int a[] = {1, 2, 3, 4, 5}, *p;
+//     p = returnPointer(a);
+//     printf("%d\n", *p);
+// }
+
+// int* returnPointer(int a[])
+// {
+//     a = a + 2;
+//     return a;
+// }
+
 #include <stdio.h>
 
-int* returnPointer(int[]);
-
-void main()
-{
-    int a[] = {1, 2, 3, 4, 5}, *p;
-    p = returnPointer(a);
-    printf("%d\n", *p);
-}
-
-int* returnPointer(int a[])
-{
-    a = a + 2;
-    return a;
-}
