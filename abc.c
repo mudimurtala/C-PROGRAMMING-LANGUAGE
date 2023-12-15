d34552c8ab8de1d69553f4c1bf224f6c9890e764
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 07:27:05 +0100

    c programming

--- abc.c
+++ abc.c
@@ -263,26 +263,29 @@
 //     printf("Both the strings are : %s and %s\n", str1, str2);
 // }
 
+// #include <stdio.h>
+
+// void modify(char*, char*);
+
+// void main()
+// {
+//     char str1[] = "jenny";
+//     char str2[] = "khatri";
+//     modify(str1, str2);
+
+// }
+
+// void modify(char* str1, char* str2)
+// {
+//     int i, l = 0;
+//     for(i = 0; str1[i] != '\0'; i++)
+//     {
+//         l = l + 1;
+//     }
+//     str2[1] = 'z';
+//     printf("Length of string 1 is : %d\n", l);
+//     printf("Both the strings are : %s and %s\n", str1, str2);
+// }
+
 #include <stdio.h>
 
-void modify(char*, char*);
-
-void main()
-{
-    char str1[] = "jenny";
-    char str2[] = "khatri";
-    modify(str1, str2);
-
-}
-
-void modify(char* str1, char* str2)
-{
-    int i, l = 0;
-    for(i = 0; str1[i] != '\0'; i++)
-    {
-        l = l + 1;
-    }
-    str2[1] = 'z';
-    printf("Length of string 1 is : %d\n", l);
-    printf("Both the strings are : %s and %s\n", str1, str2);
-}
