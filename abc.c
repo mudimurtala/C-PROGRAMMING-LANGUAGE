6ae9eaec358d0db02f69b7c717db71169cfd1612
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 06:54:49 +0100

    c programming

--- abc.c
+++ abc.c
@@ -213,28 +213,31 @@
 //     }
 // }
 
+// #include <stdio.h>
+
+// int avg(int[], int);
+
+// void main()
+// {
+//     int marks[5] = {10, 20, 30, 40, 50}, size, average;
+//     size = sizeof(marks)/sizeof(marks[0]);
+//     average = avg(marks, size);
+//     printf("average = %d\n", average);
+//     printf("Inside main size of array is (in bytes) : %ld\n", sizeof(marks));
+// }
+
+// int avg(int marks1[], int size)
+// {
+//     int i, sum, average;
+//     for(i = 0; i < size; i++)
+//     {
+//         sum = sum + marks1[i];
+//     }
+//     average = sum/size;
+//     printf("Inside avg function size of array is (in bytes) : %ld\n", sizeof(marks1));
+
+//     return average;
+// }
+
 #include <stdio.h>
 
-int avg(int[], int);
-
-void main()
-{
-    int marks[5] = {10, 20, 30, 40, 50}, size, average;
-    size = sizeof(marks)/sizeof(marks[0]);
-    average = avg(marks, size);
-    printf("average = %d\n", average);
-    printf("Inside main size of array is (in bytes) : %ld\n", sizeof(marks));
-}
-
-int avg(int marks1[], int size)
-{
-    int i, sum, average;
-    for(i = 0; i < size; i++)
-    {
-        sum = sum + marks1[i];
-    }
-    average = sum/size;
-    printf("Inside avg function size of array is (in bytes) : %ld\n", sizeof(marks1));
-
-    return average;
-}
