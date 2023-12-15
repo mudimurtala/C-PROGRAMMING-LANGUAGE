39d42a0b7b9e3b3f022a05ba4edb1cad1f865246
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 07:18:11 +0100

    c programming

--- abc.c
+++ abc.c
@@ -239,6 +239,30 @@
 //     return average;
 // }
 
+// #include <stdio.h>
+
+// void modify(char[], char[]);
+
+// void main()
+// {
+//     char str1[] = "jenny";
+//     char str2[] = "khatri";
+//     modify(str1, str2);
+
+// }
+
+// void modify(char str1[], char str2[])
+// {
+//     int i, l = 0;
+//     for(i = 0; str1[i] != '\0'; i++)
+//     {
+//         l = l + 1;
+//     }
+//     str2[1] = 'z';
+//     printf("Length of string 1 is : %d\n", l);
+//     printf("Both the strings are : %s and %s\n", str1, str2);
+// }
+
 #include <stdio.h>
 
 void modify(char[], char[]);
