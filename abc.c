973a4d436b92c95c7f86e71c4a3353611d0982c9
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 14:31:43 +0100

    c programming

--- abc.c
+++ abc.c
@@ -395,4 +395,27 @@
 // }
 
 #include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    //size --> what do we put?
+    //void* --> how can we get a stronger type?
+    //printf("%lu\n", sizeof(int) * 10);
+    int* array = malloc(sizeof(int) * 10);
+
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = i;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%i ", array[i]);
+    }
+
+    printf("\n");
+
+    free(array);
+    return 0;
+}
