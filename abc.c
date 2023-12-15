f4946925a8bd08d84c199bb81f99d84da5b7fd54
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 07:15:35 +0100

    c programming

--- abc.c
+++ abc.c
@@ -241,3 +241,24 @@
 
 #include <stdio.h>
 
+void modify(char[], char[]);
+
+void main()
+{
+    char str1[] = "jenny";
+    char str2[] = "khatri";
+    modify(str1, str2);
+
+}
+
+void modify(char str1[], char str2[])
+{
+    int i, l = 0;
+    for(i = 0; str1[i] != '\0'; i++)
+    {
+        l = l + 1;
+    }
+    str2[1] = 'z';
+    printf("Length of string 1 is : %d\n", l);
+    printf("Both the strings are : %s %s", str1, str2);
+}
