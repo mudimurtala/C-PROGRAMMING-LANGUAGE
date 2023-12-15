07781fd403917c098a5fc44f1ca1a36f8dc85e7b
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-15 19:50:07 +0100

    c programming

--- abc.c
+++ abc.c
@@ -440,20 +440,34 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-int main(void)
-{
-    int *a = malloc(sizeof(int) * 10);
+// void memory_hog(int size);
 
-    for (int i = 0; i < 10; i++)
-    a[i] = 10 - i;
+// int main(void)
+// {
+//     int *a = malloc(sizeof(int) * 10);
 
-    for (int i = 0; i < 10; i++)
-    printf("a[%d] = %d\n", i, a[i]);
-    printf("\n");
-    printf("a: %p\n", a);
-    free(a);
-    return 0;
-}
+//     for (int i = 0; i < 10; i++)
+//     a[i] = 10 - i;
+
+//     for (int i = 0; i < 10; i++)
+//     printf("a[%d] = %d\n", i, a[i]);
+//     printf("\n");
+//     printf("a: %p\n", a);
+//     free(a);
+    
+
+//    //while (1)
+//    memory_hog(128000);
+//     return 0;
+// }
+
+// void memory_hog(int size)
+// {
+//     int *a = malloc(size);
+//     free(a);
+// }
+
+
