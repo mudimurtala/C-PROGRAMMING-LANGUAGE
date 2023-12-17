35489d0053daae26bd97740e1f63caa56ae6dba5
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 17:47:30 +0100

    c programming

--- abc.c
+++ abc.c
@@ -869,9 +869,25 @@
 //     ptr->link = temp;
 // }
 
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node
+{
+    int data;
+    struct node *link;
+};
 
+struct node* add_at_end(struct node *ptr, int data)
+{
+    struct node *temp = malloc(sizeof(struct node));
+    temp->data = data;
+    temp->link = NULL;
 
+    ptr->link = temp;
+    return temp;
+
+}
 
 
 
