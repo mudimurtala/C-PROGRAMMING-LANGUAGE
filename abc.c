7b224fa98dca07b9ecdb3582a7f7946e33e4969d
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 07:13:01 +0100

    c programming

--- abc.c
+++ abc.c
@@ -654,5 +654,19 @@
 
 
 
+#include <stdio.h>
 
+struct abc {
+    int x;
+    int y;
+    int z;
+};
+
+int main()
+{
+    struct abc a = {.z = 90, .y = 60, .x = 30};
+    printf("%d, %d, %d", a.x, a.y, a.z);
+
+    return 0;
+}
 
