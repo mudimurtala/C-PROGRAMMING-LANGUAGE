02b0d22b8ed773f04c035c11bfbcb42567d18074
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 17:39:09 +0100

    c programming

--- abc.c
+++ abc.c
@@ -838,6 +838,39 @@
 // }
 
 
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// struct node
+// {
+//     int data;
+//     struct node *link;
+// };
+
+// int main()
+// {
+//     add_at_end(head, 67);
+// }
+
+
+// void add_at_end(struct node *head, int data)
+// {
+//     struct node *ptr, *temp;
+//     ptr = head;
+//     temp = (struct node *)malloc(sizeof(struct node));
+
+//     temp->data = data;
+//     temp->link = NULL;
+
+//     while (ptr->link == NULL)
+//     {
+//         ptr = ptr->link;
+//     }
+//     ptr->link = temp;
+// }
+
+
+
 
 
 
