7ab8a4550fffc8d2e2f83aae96fb23cb14f9fc18
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 12:27:33 +0100

    c programming

--- abc.c
+++ abc.c
@@ -709,18 +709,22 @@
 
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-struct abc {
-    int x;
-    int y;
-};
+// struct abc {
+//     int x;
+//     int y;
+// };
 
-int main()
-{
-    struct abc a = {0, 1};
-    struct abc *ptr = &a;
-    printf("%d %d\n", ptr->x, ptr->y);
-    return 0;
-}
+// int main()
+// {
+//     struct abc a = {0, 1};
+//     struct abc *ptr = &a;
+//     printf("%d %d\n", ptr->x, ptr->y);
+//     return 0;
+// }
+
+
+
+
 
