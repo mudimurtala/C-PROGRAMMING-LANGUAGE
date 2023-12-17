a5131a703604518842b72ecae5f7778e846da9d5
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 14:10:38 +0100

    c programming

--- abc.c
+++ abc.c
@@ -753,7 +753,7 @@ int main()
 
     struct Ournode p = {'1', '0', 'a' + 2};
     struct Ournode *q = &p;
-    printf("%c %c", *((char*) q + 1), *((char*) q + 2));
+    printf("%c %c\n", *((char*) q + 1), *((char*) q + 2));
 
     return 0;
 }
