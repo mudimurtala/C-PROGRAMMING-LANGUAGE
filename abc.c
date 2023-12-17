eddc39b9bb390c312d165888a83de9fb45f3cb9f
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 06:07:57 +0100

    c programming

--- abc.c
+++ abc.c
@@ -522,29 +522,29 @@
 //     return (ptStr);
 // }
 
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-int main()
-{
-    int n, i, *ptr;
-    printf("Enter total no of values : ");
-    scanf("%d", &n);
+// int main()
+// {
+//     int n, i, *ptr;
+//     printf("Enter total no of values : ");
+//     scanf("%d", &n);
 
-    ptr = (int *)malloc(n * sizeof(int));
+//     ptr = (int *)malloc(n * sizeof(int));
 
-    printf("\nEnter the values : ");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", (ptr + i));
-    }
+//     printf("\nEnter the values : ");
+//     for (i = 0; i < n; i++)
+//     {
+//         scanf("%d", (ptr + i));
+//     }
 
-    printf("\nThe entered values are : ");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\n", *(ptr + i));
-    }
+//     printf("\nThe entered values are : ");
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("%d\n", *(ptr + i));
+//     }
 
-    free(ptr);
-}
+//     free(ptr);
+// }
 
