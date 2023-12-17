a1e44d9df419ea4da6500ad3189f4021be2fd24c
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 06:33:06 +0100

    c programming

--- abc.c
+++ abc.c
@@ -548,21 +548,57 @@
 //     free(ptr);
 // }
 
+// #include <stdio.h>
+
+// struct {
+//     char *name;
+//     int age;
+//     int salary;
+// } emp1, emp2;
+
+// int manager()
+// {
+//     struct {
+//         char *name;
+//         int age;
+//         int salary;
+//     } manager;
+
+//     manager.age = 27;
+
+//     if (manager.age > 30)
+//         manager.salary = 65000;
+//     else
+//         manager.salary = 55000;
+//     return manager.salary;
+// }
+
+// int main()
+// {
+//     printf("Enter the salary of employee1: ");
+//     scanf("%d", &emp1.salary);
+//     printf("Enter the salary of employee2: ");
+//     scanf("%d", &emp2.salary);
+//     printf("Employee 1 salary is: %d\n", emp1.salary);
+//     printf("Employee 2 salary is: %d\n", emp2.salary);
+//     printf("Manager's salary is: %d\n", manager());
+//     return 0;
+
+
+// }
+
+
 #include <stdio.h>
 
-struct {
+struct employee{
     char *name;
     int age;
     int salary;
-} emp1, emp2;
+};
 
 int manager()
 {
-    struct {
-        char *name;
-        int age;
-        int salary;
-    } manager;
+    struct employee manager;
 
     manager.age = 27;
 
@@ -575,6 +611,8 @@ int manager()
 
 int main()
 {
+    struct employee emp1;
+    struct employee emp2;
     printf("Enter the salary of employee1: ");
     scanf("%d", &emp1.salary);
     printf("Enter the salary of employee2: ");
