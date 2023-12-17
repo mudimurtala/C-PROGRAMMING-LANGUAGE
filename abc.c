6f21464b94375b0c753685aa04175379f4b46c39
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-17 17:58:47 +0100

    c programming

--- abc.c
+++ abc.c
@@ -892,3 +892,30 @@ struct node* add_at_end(struct node *ptr, int data)
 
 
 
+
+int main()
+{
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *ptr =head;
+    ptr = add_at_end(ptr, 98);
+    ptr = add_at_end(ptr, 3);
+    ptr = add_at_end(ptr, 67);
+
+    ptr = head;
+
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+
+    return 0;
+
+}
+
+
+
+
