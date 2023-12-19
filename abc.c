5592674dfd4f478c9a6fd7e2287e477e85f03736
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-19 09:28:33 +0100

    c programming

--- abc.c
+++ abc.c
@@ -917,7 +917,117 @@
 // }
 
 
+// #include <stdio.h>
+// #define LIMIT 100
 
+// int main()
+// {
+//     void push();
+//     {
+//         int stack[LIMIT], top, element;
+//         if(top == LIMIT- 1)
+//         {
+//             printf("Stack Overflow\n");
+//         }
+//         else
+//         {
+//             printf("Enter the element to be inserted:");
+//             scanf("%d", &element);
+//             top++;
+//             stack[top]=element;
+//         }
+//     }
+// }
 
+#include <stdio.h>
+#include <stdlib.h>
+#define LIMIT 100 // specifying the maximum limit of the stack
 
+/* Global declaration of variables */
 
+int stack[LIMIT]; // Array imlementation of stack
+int top; // to insert and delete the data elements in the stack
+int i; // to traverse the loop to while displaying the
+int choice; // to choose either of the 3 stack operations
+
+void push();
+void pop();
+void display();
+
+int main()
+{
+    printf("Welcome to DataFlair tutorials!\n\n");
+
+    printf("ARRAY IMPLEMENTATION USING STACKS\n\n");
+    top = -1;
+    do
+    {
+        printf("1. Insert\n2. Delete\n3. Display\n4. Exit\n\n");
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+            case 1:
+            push();
+            break;
+            case 2:
+            pop();
+            break;
+            case 3:
+            display();
+            break;
+            case 4:
+            exit(0);
+            default:
+            printf("Sorry, Invald choise!\n");
+            break;
+        }
+    }
+
+    while (choice != 4);
+    return 0;
+}
+
+void push()
+{
+    int element;
+    if(top == LIMIT -1)
+    {
+        printf("Stack underflow\n");
+    }
+    else
+    {
+        printf("Enter the element to be inserted: ");
+        scanf("%d", &element);
+        top++;
+        stack[top] = element;
+    }
+}
+
+void pop()
+{
+    int element;
+    if(top == -1)
+    {
+        printf("Stack underflow\n");
+    }
+    else
+    {
+        element = stack[top];
+        printf("The deleted itemis %d\n", stack[top]);
+        top--;
+    }
+}
+
+void display()
+{
+    if(top == -1)
+    {
+        printf("The elements of the stack are: \n");
+        for (i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
