986e6f1a7de6e97e4872f0798bde89df5e64c186
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-20 10:59:23 +0100

    stack in C

--- at_stack/stack.c
+++ at_stack/stack.c
@@ -16,15 +16,43 @@ void push(int data)
     stack_arr[top] = data;
 }
 
+int pop()
+{
+    int value;
+    if (top == -1)
+    {
+        printf("stack underflow\n");
+        exit(1);
+    }
+    value = stack_arr[top];
+    top = top -1;
+    return value;
+}
+
+void print()
+{
+    int i;
+    if (top == -1)
+    {
+        printf("stack underflow\n");
+        return;
+    }
+    for (i = top; i >= 0; i--)
+    {
+        printf("%d ", stack_arr[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
+    int data;
     push(13);
     push(22);
     push(40);
     push(50);
-    for (int i = 0; i < MAX; i++)
-    {
-        printf("%d\n", stack_arr[i]);
-    }
+    data = pop();
+    data = pop();
+    print();
     return 0;
 }
