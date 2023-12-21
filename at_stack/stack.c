4ccc95ad8622ca084777bb0587245a2a09034086
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-21 14:09:26 +0100

    stack in C

--- at_stack/stack.c
+++ at_stack/stack.c
@@ -57,40 +57,48 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-#include <stdlib.h> // for exit () function
-#define MAX 5
 
-int stack_arr[MAX];
-int first = -1;
 
-void push(int data)
-{
-    int i;
-    first += 1;
-    for (i = first; i > 0; i--)
-        stack_arr[i] = stack_arr[i - 1];
-    stack_arr[0] = data;
-}
 
-void print()
-{
-    int i;
-    if (first == -1)
-    {
-        printf("Stack underflow\n");
-        exit(1);
-    }
-    for (i = 0; i <= first; i++)
-        printf("%d ", stack_arr[i]);
-    printf("\n");
-}
 
-int main()
-{
-    push(30);
-    push(90);
-    push(40);
-    print();
-    return 0;
-}
+// #include <stdio.h>
+// #include <stdlib.h> // for exit () function
+// #define MAX 5
+
+// int stack_arr[MAX];
+// int first = -1;
+
+// void push(int data)
+// {
+//     int i;
+//     first += 1;
+//     for (i = first; i > 0; i--)
+//         stack_arr[i] = stack_arr[i - 1];
+//     stack_arr[0] = data;
+// }
+
+// void print()
+// {
+//     int i;
+//     if (first == -1)
+//     {
+//         printf("Stack underflow\n");
+//         exit(1);
+//     }
+//     for (i = 0; i <= first; i++)
+//         printf("%d ", stack_arr[i]);
+//     printf("\n");
+// }
+
+// int main()
+// {
+//     push(30);
+//     push(90);
+//     push(40);
+//     print();
+//     return 0;
+// }
+
+
+
+
