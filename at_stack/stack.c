7aa277848fb7c1b96b3ae88a953d5e8e412e1695
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-22 06:42:34 +0100

    c

--- at_stack/stack.c
+++ at_stack/stack.c
@@ -132,3 +132,23 @@
 // }
 
 
+#include <stdio.h>
+
+void main()
+{
+    int marks;
+    printf("Enter marks: ");
+    scanf("%d", &marks);
+
+
+    if (marks >= 80)
+        printf("Grade is A");
+    else if (marks >= 70)
+        printf("Grade is B");
+    else if (marks >= 60)
+        printf("Grade is C");
+    else
+        printf("Grade is D");
+    printf("End of program");
+    
+}
