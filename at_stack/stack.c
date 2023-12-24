a2151d27001c436b60918279928bd048d54601b2
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-24 15:11:34 +0100

    c programming

--- at_stack/stack.c
+++ at_stack/stack.c
@@ -315,15 +315,193 @@
 
 
 
-#include <stdio.h>
+// #include <stdio.h>
 
-void main()
+// void main()
+// {
+//     char ch = 'a';
+//     while (ch)
+//     {
+//         printf("%d ,", ch);
+//         ch++;
+//     }
+    
+// }
+
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     int i = 0;
+//     do
+//     {
+//         printf("Mudi Daura");
+//         i++;
+//     } while (i > 0);
+    
+//     printf("Out of the loop");
+    
+    
+// }
+
+
+
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     for (int j = 1; j <= 60; j++)
+//     {
+//         for (int i = 1; i <= 5; i++)
+//         {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     int i = 0, j = 1;
+
+//     while (i < 3)
+//     {
+//         while (j <= 3)
+//         {
+//             printf("%d\n", j++);
+//         }
+//     printf("%d\n", i++);
+//     }
+// }
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+
+int stack_arr[MAX];
+int first = -1;
+
+int isFull()
 {
-    int i = 10;
-    while (1)
-    {
-        printf("%d", i);
-        ++i;
-    }
-    printf("End of program");
+    if (first == MAX -1)
+        return 1;
+    else
+        return 0;
 }
+
+int isEmpty()
+{
+    if (first == -1)
+        return 1;
+    else
+        return 0;
+}
+
+void push(int data)
+{
+    int i;
+    if (isFull())
+    {
+        printf("Stack Overflow\n");
+        exit(1);
+    }
+    first += 1;
+    for (i = first; i > 0; i--)
+        stack_arr[i] = stack_arr[i - 1];
+    stack_arr[0] = data;
+}
+
+int pop()
+{
+    int i, value;
+    if (isEmpty())
+    {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    value = stack_arr[0];
+    for (i = 0; i < first; i++)
+        stack_arr[i] = stack_arr[i + 1];
+    first -= 1;
+    return value;
+}
+
+int peek()
+{
+    if (isEmpty())
+    {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    return stack_arr[0];
+}
+
+void print()
+{
+    int i;
+    if (first == -1)
+    {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    for (i = 0; i <= first; i++)
+        printf("%d ", stack_arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int choice, data;
+    while(1)
+    {
+        printf("\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Print the top element\n");
+        printf("4. Print all the elements of the stack\n");
+        printf("5. Quit\n");
+        printf("Please enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                printf("Enter the element to be pushed: ");
+                scanf("%d", &data);
+                push(data);
+                break;
+
+            case 2:
+                data = pop();
+                printf("Deleted element is %d\n", data);
+                break;
+
+            case 3:
+                printf("The topmost element of the stack is %d\n", peek());
+                break;
+
+            case 4:
+                print();
+                break;
+
+            default:
+                printf("Wrong choice\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
