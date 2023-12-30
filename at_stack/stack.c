fea941f818cf2396e38ee261351c6990592ad8e8
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-30 05:18:46 +0100

    stack

--- at_stack/stack.c
+++ at_stack/stack.c
@@ -755,3 +755,36 @@
 
 
 
+// #include <stdio.h>
+// #pragma pack(1)
+// struct abc
+// {
+//     char a;
+//     int c;
+//     char b;
+// };
+
+// int main()
+// {
+//     struct abc var;
+//     printf("%ld\n", sizeof(var));
+    
+// }
+
+
+
+
+
+#include <stdio.h>
+
+struct point
+{
+    int x, y, z;
+};
+
+int main()
+{
+    struct point p1 = {.y = 0, .z = 1, .x = 2};
+    printf("%d %d %d", p1.x, p1.y, p1.z);
+    return 0;
+}
